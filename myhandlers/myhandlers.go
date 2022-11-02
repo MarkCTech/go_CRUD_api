3e81f258ea8177604596f92b8f9457076eaacbb9
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-11-01 19:14:41 -0500

    Router and Handlers, using regular expressions

---
+++ myhandlers/myhandlers.go
@@ -0,0 +1,56 @@
+package myhandlers
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+type route struct {
+	method  string
+	regex   *regexp.Regexp
+	handler http.HandlerFunc
+}
+
+var routes = []route{
+	newRoute("GET", "/", home),
+	newRoute("GET", "/contact", contact),
+}
+
+func newRoute(method, pattern string, handler http.HandlerFunc) route {
+	return route{method, regexp.MustCompile("^" + pattern + "$"), handler}
+}
+
+type ctxKey struct{}
+
+func Serve(w http.ResponseWriter, r *http.Request) {
+	var allow []string
+	for _, route := range routes {
+		matches := route.regex.FindStringSubmatch(r.URL.Path)
+		if len(matches) > 0 {
+			if r.Method != route.method {
+				allow = append(allow, route.method)
+				continue
+			}
+			ctx := context.WithValue(r.Context(), ctxKey{}, matches[1:])
+			route.handler(w, r.WithContext(ctx))
+			return
+		}
+	}
+	if len(allow) > 0 {
+		w.Header().Set("Allow", strings.Join(allow, ", "))
+		http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.NotFound(w, r)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "home\n")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "contact\n")
+}
