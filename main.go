5984be523b566bac464cdf62f190f716a7595964
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-11-30 15:57:09 -0600

    Init Database struct in main

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"net/http"
 
 	"github.com/martoranam/go_site/myhandlers"
+	"github.com/martoranam/sql_db"
 )
 
 func main() {
+	database := sql_db.Dbstart("todosdb")
+	myhandlers.SqlHandlerDB = database
 	const port = 5000
 	router := http.HandlerFunc(myhandlers.Serve)
-
 	fmt.Printf("listening on port %d\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
 }
