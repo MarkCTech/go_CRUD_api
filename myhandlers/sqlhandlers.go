1168bcc121b13cecde127f35ec686de220aab7c3
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-12-03 07:24:09 -0600

    Parses Url in 1 func, to form sql Todos query

--- myhandlers/sqlhandlers.go
+++ myhandlers/sqlhandlers.go
@@ -4,52 +4,73 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 	"strings"
 
 	// "regexp"
-	"strconv"
 
 	"github.com/martoranam/sql_db"
 )
 
 var SqlHandlerDB *sql_db.Database
-var InputTask *sql_db.Task
+var InputUrlTask *sql_db.Task
 var ReturnedTask *sql_db.Task
 
-// var validPath = regexp.MustCompile("^/(todos)/([0-9]+)$")
-
-// func getField(r *http.Request, index int) string {
-// 	fields := r.Context().Value(ctxKey{}).([]string)
-// 	return fields[index]
-// }
-
-type AllTasksPage struct {
+type TasksPage struct {
 	Title    string
 	AllTasks []sql_db.Task
 }
 
-func getTodos(w http.ResponseWriter, r *http.Request) { //8
+// func getAllTodos(w http.ResponseWriter, r *http.Request) {
+// 	defer r.Body.Close()
+
+// 	allTasks := sql_db.GetAllTasks(SqlHandlerDB.Db)
+
+// 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+// 	p := TasksPage{Title: "Displaying All Tasks:", AllTasks: allTasks}
+// 	t, _ := template.ParseFiles("html/alltaskstemplate.html")
+// 	fmt.Println(r)
+// 	fmt.Println(t.Execute(w, p))
+// }
+
+func getUrlTodos(w http.ResponseWriter, r *http.Request) { //8
 	defer r.Body.Close()
 	path := r.URL.Path
 	segments := strings.Split(path, "/")
-	InputTask.Id = 0
-	var allTasks []sql_db.Task
-	if len(segments) > 2 {
-		InputTask.Id, _ = strconv.Atoi(segments[2])
-		allTasks = sql_db.GetTaskbyId(SqlHandlerDB.Db, InputTask)
-	}
-	if InputTask.Id == 0 {
-		allTasks = sql_db.GetAllTasks(SqlHandlerDB.Db)
-	}
+
 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
-	p := AllTasksPage{Title: "Displaying Tasks:", AllTasks: allTasks}
-	t, _ := template.ParseFiles("html/alltaskstemplate.html")
+	var allTasks []sql_db.Task
+
+	// If url longer than 2 segments:
+	//
+	if len(segments) > 2 {
+		urlIndex, err := strconv.Atoi(segments[2])
+		if err != nil {
+			InputUrlTask.Title = segments[2]
+			allTasks = sql_db.GetTaskbyTitle(SqlHandlerDB.Db, InputUrlTask)
+			p := TasksPage{Title: "Displaying Tasks by Title:", AllTasks: allTasks}
+			t, _ := template.ParseFiles("html/tasksbyurltemplate.html")
+			fmt.Println(r)
+			fmt.Println(t.Execute(w, p))
+			return
+		}
+
+		if InputUrlTask.Id != urlIndex {
+			InputUrlTask.Id = urlIndex
+			allTasks = sql_db.GetTaskbyId(SqlHandlerDB.Db, InputUrlTask)
+			p := TasksPage{Title: "Displaying Tasks by ID:", AllTasks: allTasks}
+			t, _ := template.ParseFiles("html/tasksbyurltemplate.html")
+			fmt.Println(r)
+			fmt.Println(t.Execute(w, p))
+			InputUrlTask.Id = 0
+			return
+		}
+	}
+	allTasks = sql_db.GetAllTasks(SqlHandlerDB.Db)
+	p := TasksPage{Title: "Displaying All Tasks:", AllTasks: allTasks}
+	t, _ := template.ParseFiles("html/tasksbyurltemplate.html")
 	fmt.Println(r)
 	fmt.Println(t.Execute(w, p))
-}
-
-type IdTasksPage struct {
-	AllTasksPage
 }
 
 // func getTodosByTitle(w http.ResponseWriter, r *http.Request) {
@@ -63,9 +84,9 @@ type IdTasksPage struct {
 // }
 
 func completeTodosById(w http.ResponseWriter, r *http.Request) {
-	sql_db.CompleteTask(SqlHandlerDB.Db, InputTask)
+	sql_db.CompleteTask(SqlHandlerDB.Db, InputUrlTask)
 }
 
 func addTodo(w http.ResponseWriter, r *http.Request) {
-	sql_db.AddTask(SqlHandlerDB.Db, InputTask)
+	sql_db.AddTask(SqlHandlerDB.Db, InputUrlTask)
 }
